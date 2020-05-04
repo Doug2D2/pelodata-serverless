5c7c79ba38db2463da202f679015d5a3f5750633
Matt Vella <mattvella07@gmail.com>
2020-05-03 23:41:37 -0500

    getPrograms

---
+++ services/getPrograms/getPrograms.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func getItemByID(db *dynamodb.DynamoDB, tableName, userID, programID string) (events.APIGatewayProxyResponse, error) {
+	getItemInput := &dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"Id": {S: aws.String(programID)},
+		},
+	}
+	getItemOutput, err := db.GetItem(getItemInput)
+	if err != nil {
+		errBody := fmt.Sprintf(`{
+			"status": %d,
+			"message": "Unable to get program: %s"
+		}`, http.StatusInternalServerError, err.Error())
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       errBody,
+		}, nil
+	}
+
+	// If program is not public or created by the user then they don't have access
+	if getItemOutput.Item["Public"].BOOL == aws.Bool(false) && getItemOutput.Item["CreatedBy"].S != &userID {
+		errBody := fmt.Sprintf(`{
+			"status": %d,
+			"message": "Unauthorized to view this program"
+		}`, http.StatusUnauthorized)
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusUnauthorized,
+			Body:       errBody,
+		}, nil
+	}
+
+	reply, err := json.Marshal(getItemOutput.Item)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+		}, fmt.Errorf("Unable to marshal response: %s", err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(reply),
+	}, nil
+}
+
+func getAllItems(db *dynamodb.DynamoDB, tableName, userID string) (events.APIGatewayProxyResponse, error) {
+	scanInput := &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+		ExpressionAttributeNames: map[string]*string{
+			"#P": aws.String("Public"),
+		},
+		FilterExpression: aws.String("#P = :public or CreatedBy = :createdBy"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":public":    {BOOL: aws.Bool(true)},
+			":createdBy": {S: aws.String(userID)},
+		},
+	}
+	scanOutput, err := db.Scan(scanInput)
+	if err != nil {
+		errBody := fmt.Sprintf(`{
+			"status": %d,
+			"message": "Unable to get existing programs: %s"
+		}`, http.StatusInternalServerError, err.Error())
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       errBody,
+		}, nil
+	}
+
+	reply, err := json.Marshal(scanOutput.Items)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+		}, fmt.Errorf("Unable to marshal response: %s", err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(reply),
+	}, nil
+}
+
+func getPrograms(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+	// Get UserID header
+	userID, ok := request.Headers["UserID"]
+	userID = strings.TrimSpace(userID)
+	if !ok || userID == "" {
+		errBody := fmt.Sprintf(`{
+			"status": %d,
+			"message": "UserID header is required"
+		}`, http.StatusBadRequest)
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       errBody,
+		}, nil
+	}
+
+	// Get db region and name from env
+	tableRegion, exists := os.LookupEnv("table_region")
+	if !exists {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+		}, errors.New("table_region env var doesn't exist")
+	}
+	tableName, exists := os.LookupEnv("table_name")
+	if !exists {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+		}, errors.New("table_name env var doesn't exist")
+	}
+
+	programID, _ := request.PathParameters["programId"]
+	programID = strings.TrimSpace(programID)
+
+	sess := session.Must(session.NewSession())
+	config := &aws.Config{
+		Endpoint: aws.String(fmt.Sprintf("dynamodb.%s.amazonaws.com", tableRegion)),
+		Region:   aws.String(tableRegion),
+	}
+	db := dynamodb.New(sess, config)
+
+	if len(programID) > 0 {
+		return getItemByID(db, tableName, userID, programID)
+	}
+
+	return getAllItems(db, tableName, userID)
+}
+
+func main() {
+	lambda.Start(getPrograms)
+}
