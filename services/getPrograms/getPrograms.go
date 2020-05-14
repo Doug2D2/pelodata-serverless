a13c56844597d7aba973ad2d7ae01edd47e1c791
Matt Vella <mattvella07@gmail.com>
2020-05-13 21:57:42 -0500

    getPrograms

--- services/getPrograms/getPrograms.go
+++ services/getPrograms/getPrograms.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -15,6 +16,71 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
+
+type workout struct {
+	ID              string  `json:"id"`
+	Title           string  `json:"title"`
+	Description     string  `json:"description"`
+	Difficulty      float32 `json:"difficulty_estimate"`
+	Duration        int     `json:"duration"`
+	ImageURL        string  `json:"image_url"`
+	InstructorID    string  `json:"instructor_id"`
+	InstructorName  string  `json:"instructor_name"`
+	OriginalAirTime int64   `json:"original_air_time"`
+}
+
+type customProgram struct {
+	ID              string      `json:"id"`
+	Name            string      `json:"name"`
+	Description     string      `json:"description"`
+	Public          bool        `json:"public"`
+	EquipmentNeeded []string    `json:"equipmentNeeded"`
+	NumWeeks        int         `json:"numWeeks"`
+	Workouts        [][]workout `json:"workouts"`
+	CreatedBy       string      `json:"createdBy"`
+	CreatedDate     string      `json:"createdDate"`
+}
+
+func formatOutput(item map[string]*dynamodb.AttributeValue) (customProgram, error) {
+	program := customProgram{}
+	var err error
+
+	if item["Id"].S != nil {
+		program.ID = *item["Id"].S
+	}
+	if item["Name"].S != nil {
+		program.Name = *item["Name"].S
+	}
+	if item["Description"].S != nil {
+		program.Description = *item["Description"].S
+	}
+	if item["Public"].BOOL != nil {
+		program.Public = *item["Public"].BOOL
+	}
+	if item["CreatedBy"].S != nil {
+		program.CreatedBy = *item["CreatedBy"].S
+	}
+	if item["CreatedDate"].S != nil {
+		program.CreatedDate = *item["CreatedDate"].S
+	}
+	if item["EquipmentNeeded"].SS != nil {
+		for _, en := range item["EquipmentNeeded"].SS {
+			program.EquipmentNeeded = append(program.EquipmentNeeded, *en)
+		}
+	}
+	if item["NumWeeks"].N != nil {
+		program.NumWeeks, err = strconv.Atoi(*item["NumWeeks"].N)
+		if err != nil {
+			return customProgram{}, fmt.Errorf("Unable to convert NumWeeks to int: %s", err)
+		}
+	}
+	err = json.Unmarshal(item["Workouts"].B, &program.Workouts)
+	if err != nil {
+		return customProgram{}, fmt.Errorf("Unable to unmarshal response: %s", err)
+	}
+
+	return program, nil
+}
 
 func getItemByID(db *dynamodb.DynamoDB, tableName, userID, programID string) (events.APIGatewayProxyResponse, error) {
 	getItemInput := &dynamodb.GetItemInput{
@@ -36,6 +102,19 @@ func getItemByID(db *dynamodb.DynamoDB, tableName, userID, programID string) (ev
 		}, nil
 	}
 
+	// Check if no results are returned
+	if len(getItemOutput.Item) == 0 {
+		errBody := fmt.Sprintf(`{
+			"status": %d,
+			"message": "Unable to find program %s"
+		}`, http.StatusBadRequest, programID)
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       errBody,
+		}, nil
+	}
+
 	// If program is not public or created by the user then they don't have access
 	if getItemOutput.Item["Public"].BOOL == aws.Bool(false) && getItemOutput.Item["CreatedBy"].S != &userID {
 		errBody := fmt.Sprintf(`{
@@ -49,7 +128,15 @@ func getItemByID(db *dynamodb.DynamoDB, tableName, userID, programID string) (ev
 		}, nil
 	}
 
-	reply, err := json.Marshal(getItemOutput.Item)
+	// Format getItemOutput to customProgram
+	program, err := formatOutput(getItemOutput.Item)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+
+	reply, err := json.Marshal(program)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
@@ -87,7 +174,32 @@ func getAllItems(db *dynamodb.DynamoDB, tableName, userID string) (events.APIGat
 		}, nil
 	}
 
-	reply, err := json.Marshal(scanOutput.Items)
+	// Check if no results are returned
+	if len(scanOutput.Items) == 0 {
+		errBody := fmt.Sprintf(`{
+			"status": %d,
+			"message": "Unable to find any programs"
+		}`, http.StatusBadRequest)
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       errBody,
+		}, nil
+	}
+
+	// Format scanOutput to []customProgram
+	programs := []customProgram{}
+	for _, i := range scanOutput.Items {
+		p, err := formatOutput(i)
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+			}, err
+		}
+		programs = append(programs, p)
+	}
+
+	reply, err := json.Marshal(programs)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
